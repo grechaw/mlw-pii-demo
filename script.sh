95a719e1c074c25f7eacc32fd5efdc99c36ad7db
Charles Greer <cgreer@marklogic.com>
2018-05-02 20:55:16 -0700

    script update

--- script.sh
+++ script.sh
@@ -1,17 +1,18 @@
-./gradlew getCustomerHistory -PcustomerName=Carter -Prole=officer
-./gradlew getCustomerHistory -PcustomerName=Carter -Prole=clerk
+./gradlew -q getCustomerHistory -PcustomerName=Carter -Prole=officer
+./gradlew -q getCustomerHistory -PcustomerName=Carter -Prole=clerk
 ls user-config/security/roles/
 ls user-config/security/protected-paths/
 ls user-config/security/query-rolesets/
 
-vim user-config/security/roles/clerk.json
 vim plugins/entities/Customer/Customer.entity.json
-./gradlew mlLoadModules -i
-./gradlew hubGeneratePii
-
-./gradlew mlDeploySecurity
+./gradlew -q mlLoadModules 
+./gradlew -q hubGeneratePii
+./gradlew -q mlDeploySecurity
 ls user-config/security/protected-paths/
 ls user-config/security/query-rolesets/
 
-./gradlew getCustomerHistory -PcustomerName=Carter -Prole=clerk
-./gradlew getCustomerHistory -PcustomerName=Carter -Prole=officer
+vim user-config/security/roles/clerk.json
+./gradlew -q mlDeploySecurity
+
+./gradlew -q getCustomerHistory -PcustomerName=Carter -Prole=clerk
+./gradlew -q getCustomerHistory -PcustomerName=Carter -Prole=officer
